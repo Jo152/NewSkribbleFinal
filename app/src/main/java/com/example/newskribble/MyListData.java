75121caaf176773e6d93b1aabd52dc5a51eabf65
Eric Xu <shiqingxuvanier@gmail.com>
2021-05-08 15:50:14 -0400

    Modified List Data

--- app/src/main/java/com/example/newskribble/MyListData.java
+++ app/src/main/java/com/example/newskribble/MyListData.java
@@ -1,22 +1,22 @@
 package com.example.newskribble;
 
 public class MyListData{
-    private String description;
-    private int imgId;
-    public MyListData(String description) {
-        this.description = description;
-        this.imgId = android.R.drawable.ic_delete;
+    private String title;
+    private String content;
+    public MyListData(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
-    public String getDescription() {
-        return description;
+    public String getTitle() {
+        return title;
     }
-    public void setDescription(String description) {
-        this.description = description;
+    public void setTitle(String title) {
+        this.title = title;
     }
-    public int getImgId() {
-        return imgId;
+    public String getContent() {
+        return content;
     }
-    public void setImgId(int imgId) {
-        this.imgId = imgId;
+    public void setContent(String content) {
+        this.content = content;
     }
 }
